00daf9be2212c504e04b2dd51730f736f0611e71
xxyz30 <ts187579@163.com>
2025-01-12 21:07:03 +0800

    删除无用文件

--- src/netease/directory_generator.js
+++ src/netease/directory_generator.js
@@ -16,20 +16,7 @@ import rootModMainPy from "../../assets/python/modMain.py";
 import resourcePackManifest from "../../assets/manifest/resource_pack.json";
 
 /**
- * ├───BoxData
  * ├───entities
- * ├───items
- * ├───loot_tables
- * ├───netease_features
- * ├───netease_feature_rules
- * ├───Parts
- * ├───Presets
- * ├───recipes
- * ├───spawn_rules
- * ├───storyline
- * │   └───level
- * ├───structures
- * ├───trading
  * └───${}Scripts
  *     └───QuModLibs
  *         └───Systems
@@ -38,19 +25,7 @@ import resourcePackManifest from "../../assets/manifest/resource_pack.json";
 export function behaviorPackGenerator(behaviorPackPath, modelId) {
     // 生成相关文件夹
     mkdirSync(behaviorPackPath, {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "BoxData"), {recursive: true});
     mkdirSync(pathJoin(behaviorPackPath, "entities"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "items"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "loot_tables"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "netease_features"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "netease_feature_rules"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "Parts"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "Presets"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "recipes"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "spawn_rules"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "storyline", "level"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "structures"), {recursive: true});
-    mkdirSync(pathJoin(behaviorPackPath, "trading"), {recursive: true});
 
     // 将 modelId 修改为小写驼峰式
     let camelModelId = modelId.replace(/_([a-z])/g, (_, text) => text.toUpperCase());
@@ -132,31 +107,11 @@ export function resourcePackGenerator(resourcePackPath, modelId) {
     mkdirSync(resourcePackPath, {recursive: true});
     mkdirSync(pathJoin(resourcePackPath, "animations"), {recursive: true});
     mkdirSync(pathJoin(resourcePackPath, "animation_controllers"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "attachables"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "effects"), {recursive: true});
     mkdirSync(pathJoin(resourcePackPath, "entity"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "font"), {recursive: true});
     mkdirSync(pathJoin(resourcePackPath, "materials"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "models", "animation"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "models", "editor_materials"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "models", "effect"), {recursive: true});
     mkdirSync(pathJoin(resourcePackPath, "models", "entity",), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "models", "geometry"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "models", "mesh"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "models", "netease_block"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "models", "skeleton"), {recursive: true});
     mkdirSync(pathJoin(resourcePackPath, "render_controllers"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "shaders", "glsl"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "sounds"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "texts"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "textures", "blocks"), {recursive: true});
     mkdirSync(pathJoin(resourcePackPath, "textures", "entity", modelId), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "textures", "items"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "textures", "models"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "textures", "particle"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "textures", "sfxs"), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "textures", "ui", modelId), {recursive: true});
-    mkdirSync(pathJoin(resourcePackPath, "ui"), {recursive: true});
 
     // 生成随机的 pack_manifest.json
     let copyPackManifest = JSON.parse(JSON.stringify(resourcePackManifest));
